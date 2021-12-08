be0997136d589ad5f669b5dc56bec9d5bb8693a4
Sara Lins <saranicoly0@gmail.com>
2021-12-07 23:20:38 -0300

    add weekly measure

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,14 +14,22 @@ class DashboardController < ApplicationController
         render json: last_measure
     end
 
-    # def weekly_measure
-    #     require "google/cloud/firestore"
+    def weekly_measure
+        require "google/cloud/firestore"
 
-    #     firestore = Google::Cloud::Firestore.new project_id: "nile-2ae8a"
-    #     users = firestore.col "device_data"
+        firestore = Google::Cloud::Firestore.new project_id: "nile-2ae8a"
+        measured_data = firestore.col "device_data"
+        measured_data = measured_data.get.map do |measure|
+            measure.data
+        end
 
-    #     @weekly_measure = users.order(:timestamp, :desc).limit(1).get.map do |doc|
-    #         doc.data
-    #     end
-    # end
+        weekly_measure = []
+
+        measured_data.sort_by { |measure| measure[:datetime] }.reverse.each_with_index do | measure, index |
+            # this number is the number of sends in the week
+            index < 5 ? weekly_measure << measure : break
+        end
+
+        render json: weekly_measure
+    end
 end
