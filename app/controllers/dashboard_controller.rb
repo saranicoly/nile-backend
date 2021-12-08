fbfe1f2db1588c03e2feb0901dc4349f0aebec8c
Sara Lins <saranicoly0@gmail.com>
2021-12-08 09:12:12 -0300

    refactoring dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,33 +1,21 @@
 class DashboardController < ApplicationController
     def last_measure
-        firestore = firebase_connection
-        measured_data = firestore.col "device_data"
-
-        measured_data = measured_data.get.map do |measure|
-            measure.data
-        end
-
-        last_measure = measured_data.sort_by { |measure| measure[:datetime] }.last
-
-        render json: last_measure
+        render json: data.last
     end
 
     def weekly_measure
-        require "google/cloud/firestore"
-
-        firestore = Google::Cloud::Firestore.new project_id: "nile-2ae8a"
-        measured_data = firestore.col "device_data"
-        measured_data = measured_data.get.map do |measure|
-            measure.data
-        end
-
         weekly_measure = []
-
-        measured_data.sort_by { |measure| measure[:datetime] }.reverse.each_with_index do | measure, index |
-            # this number is the number of sends in the week
-            index < 5 ? weekly_measure << measure : break
-        end
+        data.reverse.each_with_index { | measure, index | index < 5 ? weekly_measure << measure : break }
 
         render json: weekly_measure
+    end
+
+    private
+
+    def data
+        firestore = firebase_connection
+        measured_data = firestore.col "device_data"
+
+        return measured_data.get.map { |measure| measure.data }.sort_by { |measure| measure[:datetime] }
     end
 end
