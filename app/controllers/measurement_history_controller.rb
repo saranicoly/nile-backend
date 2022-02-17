40a2c52c60d0a8ea87a5957d79bbf8610919bf30
Sara Lins <saranicoly0@gmail.com>
2022-02-17 09:07:10 -0300

    add pdf basis

--- app/controllers/measurement_history_controller.rb
+++ app/controllers/measurement_history_controller.rb
@@ -1,2 +1,52 @@
 class MeasurementHistoryController < ApplicationController
+    require 'prawn'
+
+    def index
+        weekly_measure = []
+        data.reverse.each_with_index { | measure, index | index < 5 ? weekly_measure << measure : break }
+
+        agreement('sara', 'sara testando o pdf', 100)
+        redirect_to '/agreement.pdf'
+    end
+
+    private
+
+    def data
+        firestore = firebase_connection
+        measured_data = firestore.col "device_data"
+
+        return measured_data.get.map { |measure| measure.data }.sort_by { |measure| measure[:datetime] }
+    end
+
+    def agreement(name, details, price)
+        # Apenas uma string aleatório para termos um corpo de texto pro contrato
+        lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum nulla id dignissim iaculis. Vestibulum a egestas elit, vitae feugiat velit. Vestibulum consectetur non neque sit amet tristique. Maecenas sollicitudin enim elit, in auctor ligula facilisis sit amet. Fusce imperdiet risus sed bibendum hendrerit. Sed vitae ante sit amet sapien aliquam consequat. Duis sed magna dignissim, lobortis tortor nec, suscipit velit. Nulla sit amet fringilla nisl. Integer tempor mauris vitae augue lobortis posuere. Ut quis tellus purus. Nullam dolor mauris, egestas varius ligula non, cursus faucibus orci sectetur non neque sit amet tristique. Maecenas sollicitudin enim elit, in auctor ligula facilisis sit amet. Fusce imperdiet risus sed bibendum hendrerit. Sed vitae ante sit amet sapien aliquam consequat."
+    
+        Prawn::Document.new(pdf_options = {:page_size => "A4", :page_layout => :portrait, :margin => [40, 75]}) do |pdf|
+          # Define a cor do traçado
+          pdf.fill_color "666666"
+          # Cria um texto com tamanho 30 PDF Points, bold alinha no centro
+          pdf.text "Agreement", :size => 32, :style => :bold, :align => :center
+          # Move 80 PDF points para baixo o cursor
+          pdf.move_down 80
+          # Escreve o texto do contrato com o tamanho de 14 PDF points, com o alinhamento justify
+          pdf.text "#{lorem_ipsum}", :size => 12, :align => :justify, :inline_format => true
+          # Move mais 30 PDF points para baixo o cursor
+          pdf.move_down 30
+          # Escreve o texto com os detalhes que o usuário entrou
+          pdf.text "#{details}", :size => 12, :align => :justify, :inline_format => true
+          # Move mais 30 PDF points para baixo o cursor
+          pdf.move_down 10
+          # Adiciona o nome com 12 PDF points, justify e com o formato inline (Observe que o <b></b> funciona para deixar em negrito)
+          pdf.text "Com o cliente: <b>#{name}</b> por R$#{price}", :size => 12, :align => :justify, :inline_format => true
+          # Muda de font para Helvetica
+          pdf.font "Helvetica"
+          # Inclui um texto com um link clicável (usando a tag link) no bottom da folha do lado esquerdo e coloca uma cor especifica nessa parte (usando a tag color)
+          pdf.text "Link Para o Manul do Prawn clicável", :size => 10, :inline_format => true, :valign => :bottom, :align => :left
+          # Inclui em baixo da folha do lado direito a data e o némero da página usando a tag page
+          pdf.number_pages "Gerado: #{(Time.now).strftime("%d/%m/%y as %H:%M")} - Página ", :start_count_at => 0, :page_filter => :all, :at => [pdf.bounds.right - 140, 7], :align => :right, :size => 8
+          # Gera no nosso PDF e coloca na pasta public com o nome agreement.pdf
+          pdf.render_file('public/agreement.pdf')
+        end
+    end
 end
